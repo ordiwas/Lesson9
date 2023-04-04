75519968c93905f16436ef585462a76e058922ef
gamer <gamer@DESKTOP-RBJVMBQ>
2023-04-03 20:36:01 -0400

    Add project files.

---
+++ Exercise1/Program.cs
@@ -0,0 +1,138 @@
+﻿namespace OperatorOverloadingEx1
+{
+    public class Calculator
+    {
+        public int number { get; set; }
+
+        // Overload unary operators ++ and -- 
+        public static Calculator operator ++(Calculator obj)
+        {
+            //obj.number++;
+            ++obj.number;
+            return obj;
+        }
+        public static Calculator operator --(Calculator obj)
+        {
+            //obj.number--;
+            --obj.number;
+            return obj;
+        }
+
+        // Overload Comparison Operators > and <
+        public static bool operator >(Calculator left, Calculator right)
+        {
+            bool larger = false;
+            if (left.number > right.number)
+                larger = true;
+            return larger;
+        }
+        
+        public static bool operator <(Calculator left, Calculator right)
+        {
+            bool smaller = false;
+            if (left.number < right.number)
+                smaller = true;
+            return smaller;
+        }
+
+        // Overload Binary Operators + and -
+        public static Calculator operator +(Calculator obj1, Calculator obj2)
+        {
+            Calculator Calc3 = new Calculator();
+            Calc3.number = obj1.number + obj2.number;
+            return Calc3;
+        }
+        public static Calculator operator -(Calculator obj1, Calculator obj2)
+        {
+            Calculator Calc3 = new Calculator();
+            Calc3.number = obj1.number - obj2.number;
+            return Calc3;
+        }
+
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            // create object array
+            Calculator[] numbers = new Calculator[10];
+            // place random numbers into array and print values
+            Console.Write("Original Numbers= ");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = new Calculator(); // creates the object
+                numbers[i].number = r.Next(1, 100);  // places a value in the object
+                Console.Write(" " + numbers[i].number);
+            }
+            Console.WriteLine();
+            // if divisible by 2, add 1 to value using operator ++ method
+            // otherwise subtract 1 from value using operator -- method
+            Console.Write("New Numbers +1 or -1= ");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i].number % 2 == 0)
+                {
+                    numbers[i].number++;
+                }
+                else
+                {
+                    numbers[i].number--;
+                }
+                Console.Write(" " + numbers[i].number);
+            }
+            Console.WriteLine();
+
+            // random Calculator object to add
+            Calculator numToAdd = new Calculator();
+            numToAdd.number = r.Next(1, 20);
+            // Using operator +, add numToAdd.number to each element in the array
+            // Print the results
+            Console.Write($"Numbers + {numToAdd.number} = ");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i].number += numToAdd.number;
+                Console.Write(" " + numbers[i].number);
+            }
+            Console.WriteLine();
+
+            // random Calculator object to subtract
+            Calculator numToSub = new Calculator();
+            numToSub.number = r.Next(1, 20);
+            // Using operator -, subtract numToSub.number from each element in the array
+            // Print the results
+            Console.Write($"Numbers - {numToSub.number}= ");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i].number -= numToSub.number;
+                Console.Write(" " + numbers[i].number);
+            }
+
+            Console.WriteLine();
+
+            // random Calculator object for comparison
+            Calculator numToCompare = new Calculator();
+            numToCompare.number = r.Next(1, 100);
+            // Using operator > and operator <, compare each element to numToCompare.number
+            // print if the element is higher, lower or equal to the number you are comparing to
+            Console.WriteLine($"Numbers above or below {numToCompare.number}");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i].number > numToCompare.number) 
+                {
+                    Console.Write(" " + numbers[i].number);
+                    Console.Write("_Higher");
+                }
+                else if (numbers[i].number < numToCompare.number)
+                {
+                    Console.Write(" " + numbers[i].number);
+                    Console.Write("_Lower");
+                }
+                else
+                {
+                    Console.Write(" " + numbers[i].number);
+                    Console.Write("_Equal");
+                }
+            }
+        }
+    }
+}
---
+++ Menu/Program.cs
@@ -0,0 +1,158 @@
+﻿namespace Menu
+{
+    public class Menu
+    {
+        public string item { get; set; }
+        public double price { get; set; }
+
+        // Overload unary operators ++ and -- 
+        public static Menu operator ++(Menu obj)
+        {
+            ++obj.price;
+            return obj;
+        }
+        public static Menu operator --(Menu obj)
+        {
+            //obj.number--;
+            --obj.price;
+            return obj;
+        }
+
+        // Overload Comparison Operators > and <
+        public static bool operator >(Menu left, Menu right)
+        {
+            bool larger = false;
+            if (left.price > right.price)
+                larger = true;
+            return larger;
+        }
+
+        public static bool operator <(Menu left, Menu right)
+        {
+            bool smaller = false;
+            if (left.price < right.price)
+                smaller = true;
+            return smaller;
+        }
+
+        // Overload Binary Operators + and -
+        public static Menu operator +(Menu obj1, Menu obj2)
+        {
+            Menu Calc3 = new Menu();
+            Calc3.price = obj1.price + obj2.price;
+            return Calc3;
+        }
+        public static Menu operator -(Menu obj1, Menu obj2)
+        {
+            Menu Calc3 = new Menu();
+            Calc3.price = obj1.price - obj2.price;
+            return Calc3;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many food menu items do you want?");
+            int m = int.Parse(Console.ReadLine());
+            Menu[] food = new Menu[m];
+            for (int i = 0; i < food.Length; i++)
+            {
+                Console.WriteLine($"Write the food item you want and it's price! {i + 1} (Two separate lines for item and price)");
+                string a = Console.ReadLine();
+                food[i] = new Menu();
+                food[i].item = a;
+                double b = double.Parse(Console.ReadLine());
+                food[i].price = b;
+                Console.Write("" + food[i].item + " $" + food[i].price);
+                Console.WriteLine();
+            }
+            
+            Console.WriteLine("How many drink menu items do you want?");
+            int d = int.Parse(Console.ReadLine());
+            Menu[] drink = new Menu[d];
+            for (int i = 0; i < drink.Length; i++)
+            {
+                Console.WriteLine($"Write the drink item you want and it's price! {i + 1} (Two separate lines for item and price)");
+                string a = Console.ReadLine();
+                drink[i] = new Menu();
+                drink[i].item = a;
+                double b = double.Parse(Console.ReadLine());
+                drink[i].price = b;
+                Console.Write("" + drink[i].item + " $" + drink[i].price);
+                Console.WriteLine();
+            }
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Food Menu");
+            for (int i = 0; i < food.Length; i++)
+            {
+                Console.WriteLine($"{food[i].item} ${food[i].price}");
+            }
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Drink Menu");
+            for (int i = 0; i < drink.Length; i++)
+            {
+                Console.WriteLine($"{drink[i].item} ${drink[i].price}");
+            }
+            Console.WriteLine("Add 1 to all the food menu prices");
+            for (int i = 0; i < food.Length; i++)
+            {
+                food[i].price++;
+            }
+            Console.WriteLine("Minus 1 to all the drink menu prices");
+            for (int i = 0; i < drink.Length; i++)
+            {
+                drink[i].price--;
+            }
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Food Menu");
+            for (int i = 0; i < food.Length; i++)
+            {
+                Console.WriteLine($"{food[i].item} ${food[i].price}");
+            }
+            Console.WriteLine("Drink Menu");
+            for (int i = 0; i < drink.Length; i++)
+            {
+                Console.WriteLine($"{drink[i].item} ${drink[i].price}");
+            }
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Find all food menu items less than 10");
+            for (int i = 0; i < food.Length; i++)
+            {
+                if (food[i].price < 10)
+                {
+                    Console.WriteLine($"{food[i].item} ${food[i].price}");
+                }
+            }
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Find all the drink menu items greater than 5");
+            for (int i = 0; i < drink.Length; i++)
+            {
+                if (drink[i].price > 5)
+                {
+                    Console.WriteLine($"{drink[i].item} ${drink[i].price}");
+                }
+            }
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Add 5 to all the food menu prices");
+            for (int i = 0; i < food.Length; i++)
+            {
+                food[i].price+= 5;
+            }
+            Console.WriteLine("Minus 5 to all the drink menu prices");
+            for (int i = 0; i < drink.Length; i++)
+            {
+                drink[i].price -= 5;
+            }
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Food Menu");
+            for (int i = 0; i < food.Length; i++)
+            {
+                Console.WriteLine($"{food[i].item} ${food[i].price}");
+            }
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("Drink Menu");
+            for (int i = 0; i < drink.Length; i++)
+            {
+                Console.WriteLine($"{drink[i].item} ${drink[i].price}");
+            }
+        }
+    }
+}
